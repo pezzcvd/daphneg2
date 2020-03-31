b127034db3007f48342a441f666d55d94c57e422
Francesco Pezzini <francesco.pezzini87@gmail.com>
2020-03-31 15:09:25 +0200

    data, data description BUGS: automatic man generation, version dependence

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' ecotypes
+#'
+#' ecotypes detailed description2
+#'
+#' @format An object of type \code{data.frame}
+"ecotypes"
+
+#' environmental
+#'
+#' environmental detailed description2
+#'
+#' @format An object of type \code{data.frame}
+"environmental"
+
+#' env_explain
+#'
+#' env_explain detailed description2
+#'
+#' @format An object of type \code{data.frame}
+"env_explain"
+
+#' phenotypical
+#'
+#' phenotypical detailed description2
+#'
+#' @format An object of type \code{data.frame}
+"phenotypical"
+
+#' phn_explain
+#'
+#' phn_explain detailed description2
+#'
+#' @format An object of type \code{data.frame}
+"phn_explain"
---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+globalVariables(c("argv", "environmental", "phenotypical",
+                  "env_explain", "phn_explain", "genotype",
+                  "ecotypes"))
